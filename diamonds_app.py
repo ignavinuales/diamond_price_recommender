04e5c9cee00d43b55074795882891ad222dd198a
Ignacio Vinuales <ignaciovinuales@Ignacios-MacBook-Pro.local>
2022-11-12 19:47:00 -0300

    auto expand sidebar

--- diamonds_app.py
+++ diamonds_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 from frontend import main
 
 if __name__ == '__main__':
-    st.set_page_config(page_title='diamond_price_recommender_app', layout='wide', initial_sidebar_state='expanded',
+    st.set_page_config(page_title='diamond_price_recommender_app', layout='wide', initial_sidebar_state='auto',
     menu_items={'About': "This is an mvp of a diamond price recommender app! 💎"})
     main()
